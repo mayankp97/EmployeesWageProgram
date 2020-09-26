b55e965ce35cfd1702514181b292d9ec580d4d8f
Emma Paris <mayankpurohit2012@gmail.com>
2020-09-26 20:50:33 +0530

    [Mayank] Add functionality for calculating daily wage

--- Program.cs
+++ Program.cs
@@ -12,6 +12,13 @@ namespace EmployeesWageProgram
             if (UseCase == 1)
                 Console.WriteLine(new Random().Next(0, 2) == 1 ? "Present" : "Absent");
             Console.ReadLine();
+            if (UseCase == 2)
+            {
+                var wagePerHour = 20;
+                var fullDayHour = 8;
+                var dailyEmployeeWage = wagePerHour * fullDayHour;
+                Console.WriteLine("Daily Employee Wage is {0}", dailyEmployeeWage);
+            }
         }
     }
 }
