a5ee8348c0ec54d756533e3ff2eb1e229085dcd0
Emma Paris <mayankpurohit2012@gmail.com>
2020-09-26 20:46:50 +0530

    [Mayank] Add functionality for checking attendance

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace EmployeesWageProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome Employee");
+
+            var UseCase = Convert.ToInt32(Console.ReadLine());
+            if (UseCase == 1)
+                Console.WriteLine(new Random().Next(0, 2) == 1 ? "Present" : "Absent");
+            Console.ReadLine();
         }
     }
 }
