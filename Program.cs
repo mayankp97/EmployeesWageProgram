f62a19c5982d728256fdcb29f11d10174fe56ab5
Emma Paris <mayankpurohit2012@gmail.com>
2020-09-29 16:42:53 +0530

    [Mayank] Refactor Implementation Correction

--- Program.cs
+++ Program.cs
@@ -8,30 +8,18 @@ namespace EmployeesWageProgram
         {
             Console.WriteLine("Welcome Employee");
 
-            var UseCase = Convert.ToInt32(Console.ReadLine());
-            var isPartTime = true;
-            if (UseCase == 1)
-                Console.WriteLine(new Random().Next(0, 2) == 1 ? "Present" : "Absent");
-            Console.ReadLine();
-            if (UseCase == 2)
-            {
-                var wagePerHour = 20;
-                var fullDayHour = 8;
-                var dailyEmployeeWage = wagePerHour * fullDayHour;
-                Console.WriteLine("Daily Employee Wage is {0}", dailyEmployeeWage);
-            }
-            if (UseCase == 3)
-            {
-                if(isPartTime==true){
-                    Console.Write("Enter Part Time Wage Per Hour : ");
-                    var partTimeWagePerHour = Convert.ToInt32(Console.ReadLine());
-                    var partTimeHour = 8;
-                    var partTimeWage = partTimeWagePerHour * partTimeHour;
-                    Console.WriteLine("Part Time Wage is {0}", partTimeWage);
-                }
-                else
-                    Console.WriteLine("Employee is not Part time", partTimeWage);
-            }
+            const int wagePerHour = 20;
+            int wageHour = 0;
+
+            var isPresent = new Random().Next(0, 2) == 1;
+            var isPartTime = new Random().Next(0, 2) == 1;
+
+            var dailyWage = 0;
+            if (isPartTime)
+                wageHour = 4;
+            else if (isPresent)
+                wageHour = 8;
+            dailyWage = wageHour * wagePerHour;
         }
     }
 }
