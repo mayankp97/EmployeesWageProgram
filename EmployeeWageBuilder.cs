951b471f9f7eb68f1156ec361a1a996098b41029
Emma Paris <mayankpurohit2012@gmail.com>
2020-10-03 11:25:05 +0530

    [Mayank] Add Functionality to save total wage

---
+++ EmployeeWageBuilder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeesWageProgram
+{
+    class EmployeeWageBuilder
+    {
+        public const int IS_Part_Time = 1;
+        public const int IS_Full_Time = 2;
+
+        private string _companyName;
+        private int _wagePerHour;
+        private int _workingDays;
+        private int _maxHoursPerMonth;
+        private int _totalEmployeeWage;
+
+        public EmployeeWageBuilder(string companyName, int wagePerHour, int workingDays, int maxHoursPerMonth)
+        {
+            _companyName = companyName;
+            _wagePerHour = wagePerHour;
+            _workingDays = workingDays;
+            _maxHoursPerMonth = maxHoursPerMonth;
+
+        }
+        public void CalculateMonthlyWage()
+        {
+            Console.WriteLine("Welcome Employee");
+
+            var wageHour = 0;
+            var totalHours = 0;
+            var totalWorkingDays = 0;
+            while (totalHours <= _maxHoursPerMonth && totalWorkingDays <= _workingDays)
+            {
+                totalWorkingDays++;
+                var empCheck = new Random().Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_Part_Time:
+                        wageHour = 4;
+                        break;
+                    case IS_Full_Time:
+                        wageHour = 8;
+                        break;
+                    default:
+                        wageHour = 0;
+                        break;
+                }
+                totalHours += wageHour;
+            }
+            _totalEmployeeWage = totalHours * _wagePerHour;
+            Console.WriteLine("Total Employee Wage for " + _companyName + " is " + _totalEmployeeWage);
+        }
+
+        public override string ToString()
+        {
+            return "Total Employee Wage for " + _companyName + " is " + _totalEmployeeWage;
+        }
+
+
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -5,44 +5,17 @@ namespace EmployeesWageProgram
     class Program
     {
 
-        public const int IS_Part_Time = 1;
-        public const int IS_Full_Time = 2;
+        
         
 
         static void Main(string[] args)
         {
-            CalculateMonthlyWage("Walmart",20,25,140);
-            CalculateMonthlyWage("Dtech", 42, 20, 100);
+            var walmart = new EmployeeWageBuilder("Walmart", 20, 21, 120);
+            var Dtech = new EmployeeWageBuilder("Dtech", 42, 20, 140);
+            walmart.CalculateMonthlyWage();
+            Dtech.CalculateMonthlyWage();
 
         }
-        public static void CalculateMonthlyWage(string companyName, int wagePerHour, int workingDays, int maxHoursPerMonth)
-        {
-            Console.WriteLine("Welcome Employee");
-
-            var wageHour = 0;
-            var monthlyWage = 0;
-            var totalHours = 0;
-            var totalWorkingDays = 0;
-            while (totalHours <= maxHoursPerMonth && totalWorkingDays <= workingDays)
-            {
-                totalWorkingDays++;
-                var empCheck = new Random().Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_Part_Time:
-                        wageHour = 4;
-                        break;
-                    case IS_Full_Time:
-                        wageHour = 8;
-                        break;
-                    default:
-                        wageHour = 0;
-                        break;
-                }
-                totalHours += wageHour;
-            }
-            monthlyWage = totalHours * wagePerHour;
-            Console.WriteLine("Employee's monthly Wage for {0} is : {1}",companyName,monthlyWage);
-        }
+        
     }
 }
